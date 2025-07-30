4892c2ea13127f38961b70c4b03745f13e4a115e
lohith reddy <lohithreddy0123@gmail.com>
2025-07-30 11:01:09 +0530

    .....

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import StockNewsAnalyzer from './components/StockSelector.js';
+import './App.css'; // make sure it includes .background-video styling
 
 function App() {
   return (
     <div>
+      <video autoPlay loop muted playsInline className="background-video">
+        <source src="/animation.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
       <StockNewsAnalyzer />
     </div>
   );
